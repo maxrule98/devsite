cd015a82607dc37eaf463835d783bdd61d7f8af2
maxrule98 <maxrule1998@gmail.com>
2020-09-07 23:27:03 +0100

    testing

--- src/js/nav.js
+++ src/js/nav.js
@@ -15,8 +15,8 @@ const navLinks = document.querySelector(".nav");
 navBtn.addEventListener("click", () => {
     navLinks.classList.toggle('hidden');
     if (navLinks.classList.length === 3) {
-        navBtn.innerText = '☰';
+        navBtn.innerHTML = '<svg class="btn--nav__icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>';
     } else {
-        navBtn.innerText = '✕';
+        navBtn.innerHTML = '<svg class="btn--nav__icon" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h13v-2H3v2zm0-5h10v-2H3v2zm0-7v2h13V6H3zm18 9.59L17.42 12 21 8.41 19.59 7l-5 5 5 5L21 15.59z"/></svg>';
     }
 });
