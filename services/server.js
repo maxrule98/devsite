62bbcd519cb340faa3a89a7599fb10975f433332
maxrule98 <maxrule1998@gmail.com>
2020-10-05 21:57:45 +0100

    changing back

--- services/server.js
+++ services/server.js
@@ -1,51 +1,27 @@
-const express = require('express')
-const app = express()
-// 
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
-// Import Routes
+/*----------*/
 const uiRoutes = require('./routes/routes-ui');
 const apiRoutes = require('./routes/routes-api');
 
-(async () => {
 
-    app.use(express.urlencoded({ extended: true }));
+(async () => {
+    if (process.env.NODE_ENV !== 'production') {
+        await require('./build');
+    }
     
-    // Call Routes
+    app.use(bodyParser.json({ limit: '1mb' }));
+    app.use(bodyParser.urlencoded({ extended: true }));
+
     uiRoutes(app);
     apiRoutes(app);
 
 
-
-    app.locals.PORT = PORT;
-    app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`))
+    app.locals.PORT = PORT
+    app.listen(PORT, () => {
+        console.log(`App is running at http://localhost:${PORT}`);
+    })
 })();
-
-
-
-// require('dotenv').config();
-// const bodyParser = require('body-parser');
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-// /*----------*/
-// const uiRoutes = require('./routes/routes-ui');
-// const apiRoutes = require('./routes/routes-api');
-
-
-// (async () => {
-//     if (process.env.NODE_ENV !== 'production') {
-//         await require('./build');
-//     }
-    
-//     app.use(bodyParser.json({ limit: '1mb' }));
-//     app.use(bodyParser.urlencoded({ extended: true }));
-
-//     uiRoutes(app);
-//     apiRoutes(app);
-
-
-//     app.locals.PORT = PORT
-//     app.listen(PORT, () => {
-//         console.log(`App is running at http://localhost:${PORT}`);
-//     })
-// })();
